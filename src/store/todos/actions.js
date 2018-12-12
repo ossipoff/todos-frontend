dc7f71fb0421d1d58a6a4284fa8e14a36fb029bf
ossipoff <ossipoff@gmail.com>
2018-12-12 01:09:27 +0100

    implemented calls to webapi for get, post, put and delete

--- src/store/todos/actions.js
+++ src/store/todos/actions.js
@@ -1,22 +1,43 @@
+import axios from 'axios'
+
+const baseUrl = 'https://localhost:5001/api'
+
 export const fetchTodos = ({ commit }) =>
   new Promise((resolve, reject) => {
-    resolve()
+    axios.get(`${baseUrl}/todos`).then(response => {
+      commit('storeTodos', response.data)
+      resolve()
+    }).catch(error => {
+      reject(error)
+    })
   })
 
 export const addTodo = ({commit}, todo) =>
   new Promise((resolve, reject) => {
-    commit('storeAddTodo', todo)
-    resolve()
+    axios.post(`${baseUrl}/todos`, { text: todo.text }).then(response => {
+      commit('storeAddTodo', todo)
+      resolve()
+    }).catch(error => {
+      reject(error)
+    })
   })
 
 export const deleteTodo = ({commit}, id) =>
   new Promise((resolve, reject) => {
-    commit('storeDeleteTodo', id)
-    resolve()
+    axios.delete(`${baseUrl}/todos/${id}`).then(response => {
+      commit('storeDeleteTodo', id)
+      resolve()
+    }).catch(error => {
+      reject(error)
+    })
   })
 
 export const updateTodo = ({commit}, todo) =>
   new Promise((resolve, reject) => {
-    commit('storeUpdateTodo', todo)
-    resolve()
+    axios.put(`${baseUrl}/todos/${todo.id}`, todo).then(response => {
+      commit('storeUpdateTodo', todo)
+      resolve()
+    }).catch(error => {
+      reject(error)
+    })
   })
